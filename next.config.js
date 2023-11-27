b2d7be6c1e5f999f55cafdcc0e8396cc26118cc6
JYC0413 <juyichen0413@foxmail.com>
2023-11-27 15:22:37 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
 
   webpack(config, { isServer, dev }) {
