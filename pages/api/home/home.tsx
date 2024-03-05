c9d0b4a2accccc0e09d6be46e787f4b26a5daecd
JYC0413 <juyichen0413@foxmail.com>
2024-03-05 19:42:25 +0800

    fix create conversation and save message in local storage

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -24,7 +24,6 @@ import {Prompt} from '@/types/prompt';
 import {Chat} from '@/components/Chat/Chat';
 import {Chatbar} from '@/components/Chatbar/Chatbar';
 import {Navbar} from '@/components/Mobile/Navbar';
-import Promptbar from '@/components/Promptbar';
 
 import HomeContext from './home.context';
 import {HomeInitialState, initialState} from './home.state';
@@ -313,17 +312,11 @@ const Home = ({
 
         if (window.innerWidth < 640) {
             dispatch({field: 'showChatbar', value: false});
-            dispatch({field: 'showPromptbar', value: false});
         }
 
         const showChatbar = localStorage.getItem('showChatbar');
         if (showChatbar) {
             dispatch({field: 'showChatbar', value: showChatbar === 'true'});
-        }
-
-        const showPromptbar = localStorage.getItem('showPromptbar');
-        if (showPromptbar) {
-            dispatch({field: 'showPromptbar', value: showPromptbar === 'true'});
         }
 
         const folders = localStorage.getItem('folders');
@@ -440,8 +433,6 @@ const Home = ({
                         <div className="flex flex-1">
                             <Chat stopConversationRef={stopConversationRef}/>
                         </div>
-
-                        {/*{selectedConversation.promptState !== 2 && <Promptbar/>}*/}
                     </div>
                 </main>
             )}
@@ -478,7 +469,6 @@ export const getStaticProps: GetStaticProps = async ({locale}) => {
                 'chat',
                 'sidebar',
                 'markdown',
-                'promptbar',
                 'settings',
             ])),
         },
